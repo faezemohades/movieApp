74119805e72c2fffa0eda339e9044d25d335709a
faezemohades <mohadesfaeze@gmail.com>
2022-08-15 00:33:44 +0430

    addRouting

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import './App.scss';
-
+import React from "react";
+import "./App.scss";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Home from "./components/home/Home";
+import MovieDetails from "./components/MovieDetails/MovieDetails";
+import PageNotFound from "./components/PageNotFound/PageNotFound";
+import Footer from './components/Footer/Footer'
 function App() {
   return (
-    <div className="App">
-     hi
+    <div className="app">
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movie/:imdbID" element={<MovieDetails />} />
+          <Route element={<PageNotFound />} />
+        </Routes>
+          <Footer />
+      </BrowserRouter>
     </div>
   );
 }
