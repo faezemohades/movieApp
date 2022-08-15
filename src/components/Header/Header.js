fbba005752e1eb18e833c5563b61424df852fbb7
faezemohades <mohadesfaeze@gmail.com>
2022-08-15 20:13:59 +0430

    headerStyle

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import user from "../../assets/images/user.png";
+import "./Header.scss";
 
 function Header() {
   return (
-    <div>
-      Header
+    <div className="header">
+      <Link to="/">
+        <div className="logo">Movie App</div>
+      </Link>
+      <div className="user-image">
+        <div>
+          <img src={user} alt="user" />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
