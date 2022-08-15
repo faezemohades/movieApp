b81b608334e9a7b1c03c14e74efb9865f60cb35c
faezemohades <mohadesfaeze@gmail.com>
2022-08-15 22:39:05 +0430

    setApiCall

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     <div className="app">
       <BrowserRouter>
         <Header />
-        <div classname="container">
+        <div className="container">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/movie/:imdbID" element={<MovieDetails />} />
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,13 +1,27 @@
-import React from 'react'
-import "./Home.scss"
-import MovieListing from '../MovieListing/MovieListing'
+import React, { useEffect } from "react";
+import "./Home.scss";
+import {APIKey} from '../common/apis/MovieApiKey'
+import MovieListing from "../MovieListing/MovieListing";
+import movieApi from "../common/apis/movieApi";
 function Home() {
+  useEffect(() => {
+    const movieText = "Harry";
+    const fetchMovies = async () => {
+      const response = await movieApi
+        .get(`?apiKey=${APIKey}&s=${movieText}&type=movie`)
+        .catch((err) => {
+          console.log("err:", err);
+        });
+        console.log('The response from API',response)
+    };
+    fetchMovies();
+  },[]);
   return (
     <div>
       <div className="banner-image"></div>
-      <MovieListing/>
+      <MovieListing />
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
