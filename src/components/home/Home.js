9bf91fcefd151d1cc10c1b05b4c6f66db821a872
faezemohades <mohadesfaeze@gmail.com>
2022-08-15 23:37:46 +0430

    setRedux

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -3,17 +3,20 @@ import "./Home.scss";
 import {APIKey} from '../common/apis/MovieApiKey'
 import MovieListing from "../MovieListing/MovieListing";
 import movieApi from "../common/apis/movieApi";
+import { useDispatch } from "react-redux";
+import { addMovies } from "../../features/movies/movieSlice";
 function Home() {
+  const movieText = "Harry";
+  const dispatch=useDispatch();
   useEffect(() => {
-    const movieText = "Harry";
     const fetchMovies = async () => {
       const response = await movieApi
         .get(`?apiKey=${APIKey}&s=${movieText}&type=movie`)
         .catch((err) => {
           console.log("err:", err);
         });
-        console.log('The response from API',response)
-    };
+       dispatch(addMovies(response.data))
+      };
     fetchMovies();
   },[]);
   return (
