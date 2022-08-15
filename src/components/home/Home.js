b9a6d78260f62e73ab12c4797a98d285f3ffaa5b
faezemohades <mohadesfaeze@gmail.com>
2022-08-15 20:41:19 +0430

    HomeComponent

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,10 +1,11 @@
 import React from 'react'
-
+import "./Home.scss"
+import MovieListing from '../MovieListing/MovieListing'
 function Home() {
   return (
     <div>
-      Home
-      
+      <div className="banner-image"></div>
+      <MovieListing/>
     </div>
   )
 }
