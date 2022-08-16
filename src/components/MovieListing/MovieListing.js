fc006b1dd52a7a37010180cd3a0ddea4238c830c
faezemohades <mohadesfaeze@gmail.com>
2022-08-16 09:40:38 +0430

    movieListingComponent

--- src/components/MovieListing/MovieListing.js
+++ src/components/MovieListing/MovieListing.js
@@ -1,11 +1,29 @@
-import React from 'react'
-
+import React from "react";
+import { getAllMovies } from "../../features/movies/movieSlice";
+import { useSelector } from "react-redux/es/exports";
+import MovieCard from "../MovieCard/MovieCard";
+import './MovieListing.scss'
 function MovieListing() {
+  const movies = useSelector(getAllMovies);
+  let renderMovies = "";
+  renderMovies =
+    movies.Response === "True" ? (
+      movies.Search.map((movie, index) => (
+        <MovieCard key={index} data={movie} />
+      ))
+    ) : (
+      <div className="movies-error">
+        <h3>{movies.error}</h3>
+      </div>
+    );
   return (
-    <div>
-      MovieListing
+    <div className="movie-wrapper">
+      <div className="movie-list">
+        <h2>Movies</h2>
+        <div className="movie-container">{renderMovies}</div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default MovieListing
+export default MovieListing;
